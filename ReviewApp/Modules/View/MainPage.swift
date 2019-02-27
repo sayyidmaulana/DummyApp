a51e2e350df4068914de823a77d3cb84e71fc5e9
sayyidmaulana <sayyid.yakin@students.amikom.ac.id>
2019-02-27 11:34:23 +0700

    second commit

---
+++ ReviewApp/Modules/View/MainPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainPage.swift
+//  ReviewApp
+//
+//  Created by mac on 27/02/19.
+//  Copyright © 2019 Sayyid Maulana. All rights reserved.
+//
+
+import UIKit
+import Photos
+
+class MainPage: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}

9ff6791a2152eb4cc5626a2cfe7cdd1cd328bb6c
sayyid maulana khakul yakin <sayyid.yakin@students.amikom.ac.id>
2019-04-03 22:01:33 +0700

    update commit

--- ReviewApp/AppDelegate.swift
+++ ReviewApp/AppDelegate.swift
@@ -15,6 +15,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(rootViewController: MainPage())
         // Override point for customization after application launch.
         return true
     }
